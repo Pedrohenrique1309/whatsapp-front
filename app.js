8175ecf70a28e56e16235d7dc9b5325b0a799ccf
Pedrohenrique1309 <pedrohenrique.jorge@outlook.com>
2025-04-17 11:09:56 -0300

    whatsApp finalizado

--- app.js
+++ app.js
@@ -16,67 +16,123 @@ async function puxarConversas(nome){
 
     const url = `https://apis-whatsapp-backend.onrender.com/v1/whatsapp/conversas-usuario-contato/nome/11987876567?nome=${nome}`
     const response = await fetch(url)
+    
     const data = await response.json()
-    const conversaEncotrada = []
 
-    data.forEach(function(item){
-        conversaEncotrada.push(item['menssagens'])
-    })
-
-
-    return conversaEncotrada
+    return data
 
 }
 
 async function  criarContato(contatoEncotrado){
+    const base = await puxarContato()
+    base.forEach(function(item){
+        const contatoPai = document.getElementById('contatoPai')
+        contatoPai.className = 'contatoPai'
 
-    const contatoPai = document.getElementById('contatoPai')
-    contatoPai.className = 'contatoPai'
+        const contato = document.createElement('div')
+        contato.className = 'contato'
 
-    const contato = document.createElement('div')
-    contato.className = 'contato'
+        contato.addEventListener('click', function(){criarConversa(item)})
 
-    contato.addEventListener('click', function(){puxarConversas(item.nome)})
+        const imagemPerfil = document.createElement('div')
+        imagemPerfil.className = 'imagemPerfil'
 
-    const imagemPerfil = document.createElement('div')
-    imagemPerfil.className = 'imagemPerfil'
+        const nomePerfil = document.createElement('div')
+        nomePerfil.className = 'nomePerfil'
 
-    const nomePerfil = document.createElement('div')
-    nomePerfil.className = 'nomePerfil'
+        const h1Nome = document.createElement('h1')
 
-    const h1Nome = document.createElement('h1')
+        //adicionando conteúdo na tag
+        h1Nome.textContent = item
+        
+        //adicionando a tag a classe 
+        nomePerfil.appendChild(h1Nome)
 
-    //adicionando conteúdo na tag
-    h1Nome.textContent = contatoEncotrado
-    
-    //adicionando a tag a classe 
-    nomePerfil.appendChild(h1Nome)
-
-    console.log(nomePerfil)
-
-    //adicionando no pai
-    contato.appendChild(imagemPerfil)
-    contato.appendChild(nomePerfil)
-    contatoPai.appendChild(contato)
+        //adicionando no pai
+        contato.appendChild(imagemPerfil)
+        contato.appendChild(nomePerfil)
+        contatoPai.appendChild(contato)
+        })
 }
 
-async function exibirContatos() {
-    const contatos = await puxarContato()
-    contatos.forEach(criarContato)
-}
-
-exibirContatos()
-
-async function criarConversa(){
 
 
-    const areaMenssagens= document.getElementById('areaMenssagens')
+
+
+async function criarConversa(nome){
+
+    const areaMenssagens = document.getElementById('areaMenssagens');
     areaMenssagens.className = 'areaMenssagens'
+    areaMenssagens.replaceChildren(''); 
 
-    const paiMenssagem = document.createElement('paiMenssagem')
-    paiMenssagem.className = 'paiMenssagem'
+    const base = await puxarConversas(nome)
+   
 
-    const menssagem = document.createElement('div')
-    menssagem.className = 'menssagem'
+    base.menssagens.forEach(function(item){
+        item.forEach(function(itemConversa){
 
+
+        if(itemConversa.sender !== "me"){         
+
+            const paiMenssagem = document.createElement('div')
+            paiMenssagem.className = 'paiMenssagem'
+        
+            const menssagem = document.createElement('div')
+            menssagem.className = 'menssagem'
+        
+            const menssagemH4 = document.createElement('h4')
+            menssagemH4.className = 'menssagemH4'
+            
+            const menssagemP = document.createElement('p')
+            menssagemP.className = 'menssagemP'
+        
+            //adicionando conteúdo na tag
+            menssagemP.textContent = itemConversa.time
+            menssagemH4.textContent = itemConversa.content
+        
+            //adicionando a tag no pai
+            menssagem.appendChild(menssagemP)
+            menssagem.appendChild(menssagemH4)
+            paiMenssagem.appendChild(menssagem)
+            areaMenssagens.appendChild(paiMenssagem)
+            
+
+
+        }else{
+
+            const paiMenssagemPropria = document.createElement('div')
+            paiMenssagemPropria.className = 'paiMenssagemPropria'
+        
+            const menssagem = document.createElement('div')
+            menssagem.className = 'menssagem'
+        
+            const menssagemH4 = document.createElement('h4')
+            menssagemH4.className = 'menssagemH4'
+            
+            const menssagemP = document.createElement('p')
+            menssagemP.className = 'menssagemP'
+        
+            //adicionando conteúdo na tag
+            menssagemP.textContent = itemConversa.time
+            menssagemH4.textContent = itemConversa.content
+        
+        
+            //adicionando a tag no pai
+            menssagem.appendChild(menssagemP)
+            menssagem.appendChild(menssagemH4)
+            paiMenssagemPropria.appendChild(menssagem)
+            areaMenssagens.appendChild(paiMenssagemPropria)
+
+  
+
+    }
+
+        })
+    
+    })
+    
+
+    
 }
+
+criarContato()
